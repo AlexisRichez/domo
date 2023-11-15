8bcb57a69e165618eab425f63cc390799a49986b
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2023-11-15 10:19:17 +0100

    Create snmp-exporter.sh

---
+++ snmp-exporter.sh
@@ -0,0 +1,7 @@
+docker pull prom/snmp-exporter
+
+docker run -d -p 9100:9116 --name snmp-exporter prom/snmp-exporter \
+  --web.listen-address=":9116" \
+  --web.telemetry-path="/metrics" \
+  --snmp.target="<TARGET_IP>" \
+  --snmp.community="<COMMUNITY_STRING>"
